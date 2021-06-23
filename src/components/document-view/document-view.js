a1340e7767546ba043d6b1c5e4a005eff2c69b71
startup-ideas-project <randomstartupproject2021@gmail.com>
2021-06-23 08:51:02 -0500

    text selection

--- src/components/document-view/document-view.js
+++ src/components/document-view/document-view.js
@@ -5,24 +5,91 @@ const DocumentView = ({document}) => {
     const [file, setFile] = useState({
         data: ""
     })
+    const [selectedState, setSelectedState] = useState({
+        text: "",
+            isDirty: false,
+            selection: '',
+            anchorNode: '?',
+            focusNode: '?',
+            selectionStart: '?',
+            selectionEnd: '?',
+            first: '',
+            middle: '',
+            last: ''
+    })
 
     useEffect(() => {
-     loadFileByID(document.key).then(data => setFile(data))   
+        if(document){
+            loadFileByID(document.key).then(data => {
+                setFile(data)
+                setSelectedState({
+                    ...selectedState,
+                    text: data.data
+                })
+            }) 
+        }  
     },[document])
 
-    const splitTextToButtons = (text) => {
-        return text.split(" ")
-    }
+    const onMouseUpHandler = (event) => {
+        event.preventDefault();
+        const selectionObj = (window.getSelection && window.getSelection());
+        const selection = selectionObj.toString();
+        const anchorNode = selectionObj.anchorNode;
+        const focusNode = selectionObj.focusNode;
+        const anchorOffset = selectionObj.anchorOffset;
+        const focusOffset = selectionObj.focusOffset;
+        const position = anchorNode.compareDocumentPosition(focusNode);
+        let forward = false;
 
+        if (position === anchorNode.DOCUMENT_POSITION_FOLLOWING) {
+            forward = true;
+        } else if (position === 0) {
+            forward = (focusOffset - anchorOffset) > 0;
+        }
+
+        let selectionStart = forward ? anchorOffset : focusOffset;
+
+        if (forward) {
+            if (anchorNode.parentNode.getAttribute('data-order')
+                && anchorNode.parentNode.getAttribute('data-order') === 'middle') {
+                selectionStart += selectedState.selectionStart;
+            }
+            if (anchorNode.parentNode.getAttribute('data-order')
+                && anchorNode.parentNode.getAttribute('data-order') === 'last') {
+                selectionStart += selectedState.selectionEnd;
+            }
+        } else {
+            if (focusNode.parentNode.getAttribute('data-order')
+                && focusNode.parentNode.getAttribute('data-order') === 'middle') {
+                selectionStart += selectedState.selectionStart;
+            }
+            if (focusNode.parentNode.getAttribute('data-order')
+                && focusNode.parentNode.getAttribute('data-order') === 'last') {
+                selectionStart += selectedState.selectionEnd;
+            }
+        }
+
+        const selectionEnd = selectionStart + selection.length;
+        const first = selectedState.text.slice(0, selectionStart);
+        const middle = selectedState.text.slice(selectionStart, selectionEnd);
+        const last = selectedState.text.slice(selectionEnd);
+
+        setSelectedState({
+            ...selectedState,
+            selection,
+            anchorNode,
+            focusNode,
+            selectionStart,
+            selectionEnd,
+            // first,
+            // middle,
+            // last
+        })
+    }
+    console.log(selectedState)
     return(
         <div>
-            {/* Convert the word to a bunch of buttons, this is a proof of concept for not
-            pretty it up later*/}
-            <p>{splitTextToButtons(file.data).map((word, index) => {
-                return (
-                    <button id={`${word}_${index}`}>{word}</button>
-                )
-            })}</p>
+            <span onMouseUp={onMouseUpHandler}>{selectedState.text}</span>
         </div>
     )
 }
